f8b7e997a6cae9e349d044c4cc776a33bda920e4
ik <ik@pixiv.com>
2015-08-11 11:53:57 +0900

    spec3: 分割したデータファイルからレシピ情報を読み込むように修正

--- recipe.rb
+++ recipe.rb
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
 
-recipe_name = 'オムライス'
-puts recipe_name
+recipe_data_file_path = './recipe-data.txt'
+recipe_file = open(recipe_data_file_path)
+
+recipe_file.each_line do |line|
+  puts line
+end
