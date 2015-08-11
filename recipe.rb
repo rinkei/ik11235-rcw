007c240827d038b7547760bd12dcb9084fabde46
ik <ik@pixiv.com>
2015-08-11 12:44:26 +0900

    spec6: コマンドライン引数でレシピIDを指定された時、そのIDのレシピのみを出力するように変更

--- recipe.rb
+++ recipe.rb
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 
+def output_id? output_recipe_id_array, target_id
+  return true if output_recipe_id_array.nil?
+
+  output_recipe_id_array.include?(target_id)
+end
+
+
 recipe_data_file_path = './recipe-data.txt'
 recipe_file = open(recipe_data_file_path)
+
+output_recipe_id = ARGV.map(&:to_i) if ARGV.size > 0
 RECIPE_ID_START_VALUE = 1
 
 recipe_id = RECIPE_ID_START_VALUE
 recipe_file.each_line do |line|
-  puts "#{recipe_id}: #{line}"
+  puts "#{recipe_id}: #{line}" if output_id?(output_recipe_id, recipe_id)
   recipe_id += 1
 end
