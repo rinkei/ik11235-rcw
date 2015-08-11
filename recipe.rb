04fee1ccf3b9ee07c7d43f204194ae6af0f59126
ik <ik@pixiv.com>
2015-08-11 12:22:46 +0900

    spec5: レシピ毎にレシピIDを付与するように変更

--- recipe.rb
+++ recipe.rb
@@ -2,7 +2,10 @@
 
 recipe_data_file_path = './recipe-data.txt'
 recipe_file = open(recipe_data_file_path)
+RECIPE_ID_START_VALUE = 1
 
+recipe_id = RECIPE_ID_START_VALUE
 recipe_file.each_line do |line|
-  puts line
+  puts "#{recipe_id}: #{line}"
+  recipe_id += 1
 end
