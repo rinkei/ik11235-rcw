a7b7a473f7528838d9b04d29ad9a54bc9857a257
Kei Hayashi <kei.h.hayashi@gmail.com>
2015-08-11 14:45:13 +0900

    Spec7: Add description to recipe

--- recipe.rb
+++ recipe.rb
@@ -7,6 +7,24 @@ def output_id? output_recipe_id_array, target_id
 end
 
 
+class Recipe
+  attr_accessor :id, :name, :description
+
+  def initialize(id, name)
+    @id = id
+    @name = name
+    @description = ''
+  end
+
+  def output
+    puts "[#{@id}] #{@name}"
+    puts "せつめい:"
+    puts @description
+    puts ""
+  end
+end
+
+
 recipe_data_file_path = './recipe-data.txt'
 recipe_file = open(recipe_data_file_path)
 
@@ -14,7 +32,34 @@ output_recipe_id = ARGV.map(&:to_i) if ARGV.size > 0
 RECIPE_ID_START_VALUE = 1
 
 recipe_id = RECIPE_ID_START_VALUE
+
+start_line_flag = true
+recipe = nil
+
+recipes = []
 recipe_file.each_line do |line|
-  puts "#{recipe_id}: #{line}" if output_id?(output_recipe_id, recipe_id)
-  recipe_id += 1
+  if line.length <= 1
+    # 空行なので保存して次のレシピへ
+    recipes << recipe
+    recipe = nil
+    start_line_flag = true
+    next
+
+  else
+    # レシピを読み込む
+    start_line_flag = false
+
+    if recipe
+      # 既存のレシピがある
+      recipe.description << line
+    else
+      # 新しいレシピ
+      recipe = Recipe.new(recipe_id, line)
+      recipe_id += 1
+    end
+  end
+end
+
+recipes.each do |recipe|
+  recipe.output if output_id?(output_recipe_id, recipe.id)
 end
